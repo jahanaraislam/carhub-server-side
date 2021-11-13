0f47eb06723fe24166e5355b72e749e7a969e5cf
jahanaraislam <jahanaraislamniha@gmail.com>
2021-11-14 00:26:44 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectId;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q1tq9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+console.log(uri)
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+client.connect(err => {
+
+    console.log("database connected")
+    // perform actions on the collection object
+    // client.close();
+  });
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Listening to port : ${port}`);
+});
